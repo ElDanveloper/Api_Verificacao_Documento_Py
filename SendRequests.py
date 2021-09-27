d1a8ab0785a900b7bb55e193b6711aed0cdd53e0
carlosajunior <carlosajunior@hotmail.com>
2021-09-27 10:20:18 -0300

    correção no envio do token

--- SendRequests.py
+++ SendRequests.py
@@ -11,9 +11,11 @@ def sendRequest(body, url, token):
     # headers = {
     #     'Authorization': 'Bearer {}'.format(response.json()["token"])
     # }
+    token = 'Bearer '+token
     headers = {
         'Authorization': '{}'.format(token)
     }
+    print(headers)
     responseApi = requests.post(url=url, json=body, headers=headers)
 
     if responseApi.status_code >= 200 and responseApi.status_code <= 299:
