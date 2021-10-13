f1d0bf68cb6cad579d83b51244884dc82ade3edc
Leandro Reboucas <danieldouradofsa@gmail.com>
2021-10-13 16:59:53 -0300

    correção

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ def find_regex(pdf_data, arquivo, file_name, token):
 
 def sendObject(obj, token, file_name):
     from SendRequests import sendRequest
-    print(file_name)
     response = sendRequest(obj, get_base_url()+"ProcessaGenericDoc", token)
     resposta = {
         "Sucess": True,
         "msg": "",
         "data": ""
     }
+ 
     if type(response) == dict:
         print(response)
         path="C:/Users/Administrator/Documents/Api_Verificacao_Documento_Py/Sucesso/"+file_name.split("/")[4][:-4].replace(".", "")+".json"
@@ -29,6 +29,7 @@ def sendObject(obj, token, file_name):
             obj["Nome"]+" foi lido e enviado com sucesso!",
         resposta["data"] = response
     else:
+        print(response)
         with open("C:/Users/Administrator/Documents/Api_Verificacao_Documento_Py/DeramErro/"+file_name.split("/")[4][:-4].replace(".", "")+".json", 'a', encoding='utf-8') as f:
             json.dump(obj, f, ensure_ascii=False, indent=4)
         resposta["msg"] = "O arquivo não foi reconhecido",
@@ -48,7 +49,7 @@ def main(pathToPdfs, token):
     }
     from ExtractTextFromPdf import FalhaNaLeituraPdf
     for file in files:
-        try:
+        try:            
             if file.endswith(".pdf") or file.endswith(".PDF"):
                 pdf_data, arquivo, file_name = pdfReader.teste_pdf_miner(
                     pathToPdfs+file)
@@ -67,6 +68,7 @@ def main(pathToPdfs, token):
                     pathToPdfs+file)
                 resposta = find_regex(pdf_data, arquivo, file_name, token)
         except FalhaNaLeituraPdf:
+            print("deu falha na leitura: ")
             os.remove(pathToPdfs+file)
     return resposta
 
@@ -75,4 +77,4 @@ token = sys.argv[2]
 main(pathToPdfs, token)
 
 if __name__ == "__main__":
-    main("C:/Apps_NodeJS/hunnocrm-api-node/uploads/", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJub21lIjoiTGVhbmRybyBSZWJvdcOnYXMiLCJjcGZfY25waiI6IjA0MTA1MDUzNTgyIiwiY29udHJhY3Rvcl9jbnBqIjoiMTExMTExMTExMTEiLCJpZCI6IjUiLCJwZXJzb25faWQiOiI0ODYiLCJlbWFpbCI6ImNvbnRhdG9AbGVhbmRyb3JlYm91Y2FzLmNvbSIsInBob25lX2NlbGwiOiI3NTk5Mjc0MjgxOSIsInR5cGVfdXNlciI6IkMiLCJwZXJmaWxfdXNlciI6IkQiLCJhY3RpdmUiOnRydWUsInVzZXJfaWRfc3VwZXJ2aXNvciI6IjEiLCJ1c2VyX3Bhc3N3b3JkIjoiJDJhJDEwJG53c2NsRTJaWVlQRUNFZVdJWjFyYk9yZkU5TUEwLnFqTUp6dGlOdVJkOGFNeG1Tcm9lWk9PIiwiZGVwYXJ0bWVudF9pZCI6IjEiLCJzaW1wbGVfbmFtZSI6IkxlYW5kcm8gUmVib3XDp2FzIiwiY2FtaW5ob19mb3RvIjoiaHR0cHM6Ly9maXJlYmFzZXN0b3JhZ2UuZ29vZ2xlYXBpcy5jb20vdjAvYi9odW5uby02ZjM0YS5hcHBzcG90LmNvbS9vLzElMkYxNTY4NjM3MDk5MjMzLTI4MTU3NjE4XzE4NDYxMTk5Mjg3MzM0MzVfMzc1MTk0MjAzOTQ2MjIxNTY4X24uanBnP2FsdD1tZWRpYSIsImNvbnRyYWN0b3IiOltdLCJjbGllbnRzIjpbXSwiaWF0IjoxNjMyNzY3MTk1LCJleHAiOjE2MzI4NTM1OTUsImNvbnRyYWN0b3JfaWQiOiIxIn0.ZhBhuBT8Kx_HfPxjI6ouXAd5g4KhY6x6IaJwEFr8z20")
+    main("C:/Apps_NodeJS/hunnocrm-api-node/uploads/", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJub21lIjoiTWFyY2lvIENvc3RhIFNpbHZhIiwiY3BmX2NucGoiOiI2MDYwMTk4MTU1MyIsImNvbnRyYWN0b3JfaWQiOiIxIiwiY29udHJhY3Rvcl9jbnBqIjoiMTEzNzgwMDQwMDAxMjQiLCJpZCI6IjEiLCJwZXJzb25faWQiOiIzMzEiLCJlbWFpbCI6Im1hcmNpb0Bwb2xsb2NvbnRhYmlsLmNvbS5iciIsInBob25lX2NlbGwiOiI3NTk5MTg2NDY5NiIsInR5cGVfdXNlciI6IkMiLCJwZXJmaWxfdXNlciI6IkQiLCJhY3RpdmUiOnRydWUsInVzZXJfaWRfc3VwZXJ2aXNvciI6IjEiLCJ1c2VyX3Bhc3N3b3JkIjoiJDJhJDEwJDJ6aDZjTHg0WjlkenVFUWVxNHd6Vi56ZzdkTTNEVXBlZ3Rpa0NONDg4bm5ac0RqbjJOUE1PIiwiZGVwYXJ0bWVudF9pZCI6IjciLCJzaW1wbGVfbmFtZSI6Ik3DoXJjaW8gQ29zdGEiLCJjYW1pbmhvX2ZvdG8iOm51bGwsImNvbnRyYWN0b3IiOlt7ImlkIjoiMSIsInBlcnNvbl9pZCI6IjExNyIsIm5vbWUiOiJQT0xMTyBDT05TVUxUT1JJQSBDT05UQUJJTCBFIFNJU1RFTUFTIExUREEiLCJjcGZfY25waiI6IjExMzc4MDA0MDAwMTI0IiwiZG9jX2Zvcm1hdCI6IjExLjM3OC4wMDQvMDAwMS0yNCIsInRpcG9faW5zY3JpY2FvIjpudWxsLCJ0aXBvIjoiRSIsImNhbWluaG9fZm90byI6Imh0dHBzOi8vaS5pbWd1ci5jb20vU1NuaUZvRC5wbmciLCJjcnQiOiIxIiwiY3JjX3N1YnNjcmlwdGlvbiI6IjAxODM5MiIsImVtYWlsIjoibWFyY2lvQHBvbGxjb250YWJpbC5jb20uYnIiLCJ3aGF0c2FwcF9idXNpbmVzcyI6Ijc1OTkxODY0Njk2IiwiaW5zdGFncmFtIjpudWxsLCJmYWNlYm9vayI6bnVsbCwieW91dHViZSI6bnVsbCwibGlua2VkaW4iOm51bGwsImdvb2dsZV9hY2NvdW50IjpudWxsLCJkYXRlX3JlZ2lzdGVyIjoiMjAxOS0wNy0xNFQxNDo1NjoyNC4wMDBaIiwiZGF0ZV9pbml0aWFsX2NvbnRyYWN0IjoiMjAxOS0wNy0xNFQxNDo1NjoyOS4wMDBaIiwiZGF0ZV9sYXN0X2NvbnRyYWN0IjoiMjAxOS0wNy0xNFQxNDo1NjozNC4wMDBaIiwiYWN0aXZlX2NvbnRyYWN0IjpudWxsLCJkZXBhcnRhbWVudG9faWQiOiI3IiwiZGVwYXJ0YW1lbnRvX3VzZXJfcmVzcCI6IjEiLCJkZXBhcnRhbWVudG9fbm9tZSI6IkFkbWluaXN0cmF0aXZvIiwiZGVwYXJ0YW1lbnRvX3RpcG8iOm51bGx9XSwiY2xpZW50cyI6W10sImlhdCI6MTYzMzA5MTc4MiwiZXhwIjoxNjMzMTc4MTgyfQ.UEWp03GR8d7qsV7a4SBTk_m7rYjXyeEdo43ciYtB7wQ")
