f78c580663720405bfbbaf81e9f1c8b7717ed479
carlosajunior <carlosajunior@hotmail.com>
2021-10-14 17:10:56 -0300

    correção

--- REGEXs/Aviso_Empreg_Indenizado.py
+++ REGEXs/Aviso_Empreg_Indenizado.py
@@ -20,7 +20,6 @@ Mes_ext = {
     "Novembro": "11",
     "Dezembro": "12"
 }
-# or re.search(r'AVISO PRÉVIO DO EMPREGADOR PARA DISPENSA DO EMPREGADO',contra_cheque) is not None
 
 
 def regex_empreg_indenizado(contra_cheque, obj_response):
@@ -36,14 +35,5 @@ def regex_empreg_indenizado(contra_cheque, obj_response):
         Mes, de, ano = findCompetencia.search(contra_cheque).group().split(" ")
         obj_response["Mes"] = Mes_ext[Mes]
         obj_response["Ano"] = ano
-        # ISSO DEPOIS VAI SAIR
-        # import json
-        # arquivo = open(obj_response["Nome"]+".txt", "w")
-        # obj_response["Mes"]=int(obj_response["Mes"])
-        # obj_response["Ano"]=int(obj_response["Ano"])
-        # obj_response["Valor"]=int(obj_response["Valor"])
-        # obj_response["Multa"]=int(obj_response["Multa"])
-        # obj_response["Total"]=int(obj_response["Total"])
-        # json.dump(obj_response,arquivo,ensure_ascii=False)
         return obj_response
     return None
