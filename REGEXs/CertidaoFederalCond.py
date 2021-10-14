eb45a9c160b7ae555b437317e731ac366c35b820
carlosajunior <carlosajunior@hotmail.com>
2021-10-14 16:44:47 -0300

    atualização na identificação do documento

--- REGEXs/CertidaoFederalCond.py
+++ REGEXs/CertidaoFederalCond.py
@@ -3,14 +3,14 @@ from REGEXs._removeMask import numberWithoutMask
 findCnpj = re.compile(r'\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}')
 findEmpresa = re.compile(r'Nome: (\w+\s)+')
 
+
 def regex_CertidaoFederalCond(contra_cheque, obj_response):
-    if re.search(r'CERTIDÃO',contra_cheque) is not None and re.search(r'DÉBITOS RELATIVOS AOS TRIBUTOS',contra_cheque) is not None: 
-        obj_response["Nome"]="CertidaoFederalCond"
-        obj_response["Tipo"]="27"
-        obj_response["Cnpj"]=numberWithoutMask(findCnpj.search(contra_cheque).group())
-        obj_response["Empresa"]=findEmpresa.search(contra_cheque).group().replace("Nome: ","").replace("\n","")
+    if re.search(r'CERTID', contra_cheque) is not None and re.search(r'NEGATIVA', contra_cheque) is not None and re.search(r'FEDERA', contra_cheque) is not None and re.search(r'BUTOS', contra_cheque) is not None:
+        obj_response["Descricao"] = "Certidao Negativa Federal de Debitos"
+        obj_response["Tipo"] = "27"
+        obj_response["Cnpj"] = numberWithoutMask(
+            findCnpj.search(contra_cheque).group())
+        obj_response["Empresa"] = findEmpresa.search(
+            contra_cheque).group().replace("Nome: ", "").replace("\n", "")
         return obj_response
     return None
-
-
-
