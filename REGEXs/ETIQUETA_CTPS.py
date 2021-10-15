01884af398d92449def29d5bc5c0c31120dd83c5
carlosajunior <carlosajunior@hotmail.com>
2021-10-15 10:08:07 -0300

    regex para documento etiqueta CTPS

---
+++ REGEXs/ETIQUETA_CTPS.py
@@ -0,0 +1,14 @@
+import re
+findEmpresa = re.compile(
+    r'Empregador:\n[a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+[-\s]*[a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]*\n')
+findCNPJ = re.compile(r'\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}')
+
+
+def etiqueta_CTPS(contra_cheque, obj_response):
+    if re.search('CBO', contra_cheque) is not None and re.search('Folha/Livro nº', contra_cheque) is not None:
+        obj_response["Tipo"] = 103
+        obj_response["Descricao"] = "Etiqueta CTPS"
+        obj_response["Cnpj"] = findCNPJ.search(contra_cheque).group()
+        obj_response["Empresa"] = findEmpresa.search(
+            contra_cheque).group().split(':\n')[1][:-1]
+        return obj_response
